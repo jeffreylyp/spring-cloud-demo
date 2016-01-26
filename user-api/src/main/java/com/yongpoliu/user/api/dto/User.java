bc42e07d3e1d3c014bdcdb5dd92773787541d2e3
liuyongpo <liuyongpo@gmail.com>
2016-01-26 16:28:06 +0800

    rm User

--- user-api/src/main/java/com/yongpoliu/user/api/dto/User.java
+++ user-api/src/main/java/com/yongpoliu/user/api/dto/User.java
@@ -1,18 +1,16 @@
 package com.yongpoliu.user.api.dto;
 
-import java.io.Serializable;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *
+ * 用户对象
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements Serializable {
+public class User {
 
   private String name;
 
